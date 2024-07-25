2af5ef2e9a596b522afd4275ebccc6748f41b569
kateincoding <kateincoding@gmail.com>
2024-07-25 07:22:21 -0500

    [tailwind]first steps

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        'primary': '#CC2D4A',
+        'secondary': '#FFD700',
+        'terciary': '#FFD700',
+      }),
+      textColor: theme => ({
+        'primary': '#CC2D4A',
+        'secondary': '#FFD700',
+        'terciary': '#FFD700',
+      }),
+      // fontFamily:{
+      //   Montserrat: ['Montserrat', 'sans-serif'],
+      // }
+    },
   },
   plugins: [],
 }
